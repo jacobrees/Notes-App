132072fcbd1d7b529daeef4e58e544b873a77ab5
jacobrees <jacob.rees97@yahoo.com>
2022-11-22 09:17:22 +0000

    Tidy up code with eslint

--- app.js
+++ app.js
@@ -1,57 +1,59 @@
 const yargs = require('yargs');
-const {addNote, removeNote, listNotes, readNote} = require('./notes.js');
+const {
+  addNote, removeNote, listNotes, readNote,
+} = require('./notes.js');
 
-yargs.version('1.0')
+yargs.version('1.0');
 
 yargs.command({
-    command: 'add',
-    describe: 'Add a new note',
-    builder: {
-        title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: 'string'
-        },
-        body: {
-            describe: 'Note body',
-            demandOption: true,
-            type: 'string'
-        }
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
     },
-    handler: (argv) => {
-        addNote(argv.title, argv.body)
-    }
-})
-
-yargs.command({
-    command: 'remove',
-    describe: 'Remove a note',
-    builder: {
-        title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: 'string'
-        }
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
     },
-    handler: (argv) => {
-        removeNote(argv.title)
-    }
-})
+  },
+  handler: (argv) => {
+    addNote(argv.title, argv.body);
+  },
+});
 
 yargs.command({
-    command: 'list',
-    describe: 'List your notes',
-    handler: () => {
-        listNotes()
-    }
-})
+  command: 'remove',
+  describe: 'Remove a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: (argv) => {
+    removeNote(argv.title);
+  },
+});
 
 yargs.command({
-    command: 'read',
-    describe: 'Read a note',
-    handler: (argv) => {
-        readNote(argv.title)
-    }
-})
+  command: 'list',
+  describe: 'List your notes',
+  handler: () => {
+    listNotes();
+  },
+});
 
-yargs.parse()
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: (argv) => {
+    readNote(argv.title);
+  },
+});
+
+yargs.parse();
